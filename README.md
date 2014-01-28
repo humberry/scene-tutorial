659552eed7a07dae3182b8be711e1652af23fc2a
humberry <brummberry@gmail.com>
2014-01-28 21:41:49 +0100

    some more...

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ beginner004.py - Demonstrates how to keep users touching the screen.<br />
 beginner005.py - Touching a rectangle changes it's color.<br />
 beginner006.py - Adding a scene.Layer to a Pythonista scene.Scene.<br />
 beginner007.py - Adding alpha and scale animation to circle button/image.<br />
+intermediate001-1.py - Open a photo from the camera roll<br />
+intermediate001-2.py - Two finger zoom in/out gesture<br />
+intermediate001-3.py - Your exercise<br />
---
+++ intermediate001-1.py
@@ -0,0 +1,26 @@
+import photos
+import scene
+
+class PhotoText(scene.Scene):
+    def __init__(self):
+        self.img = photos.pick_image()
+        self.picsize = scene.Size(*self.img.size)
+        if self.img:
+            scene.run(self)
+        else:
+            print('Good bye!')
+    def setup(self):
+        self.layer = scene.Layer(scene.Rect(0, 0, self.bounds.w, self.bounds.h))        self.layer.image = scene.load_pil_image(self.img)
+        self.add_layer(self.layer)
+        
+    def draw(self):
+        scene.background(0,0,0)
+        self.root_layer.update(self.dt)
+        self.root_layer.draw()
+        scene.fill(1,1,1)   # watch+battery -> white background
+        scene.rect(0, self.bounds.h, self.bounds.w, 20)  # watch+battery
+
+if photos.get_count():
+    PhotoText()
+else:
+    print('Sorry no access or no pictures.')
